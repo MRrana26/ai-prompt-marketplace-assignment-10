395ba8826f8786d4ac2eb7da8dc75d5b0004b4a3
MRrana26 <hafezmasudranamn@gmail.com>
2026-06-23 18:18:59 +0600

    all prompts vercel deploy promlem solved

--- src/app/dashboard/admin/all-prompts/page.jsx
+++ src/app/dashboard/admin/all-prompts/page.jsx
@@ -1,5 +1,8 @@
 "use client";
 
+
+export const dynamic = "force-dynamic";
+
 import React, { useEffect, useState } from "react";
 import { Eye, Check, X, Trash2, Star } from "lucide-react";
 import { toast } from "sonner";
